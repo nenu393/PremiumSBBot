fcdc68560c6c0c574f16513758ca391aac3c803e
nenu393 <ffp045606@gmail.com>
2024-11-25 21:03:15 +0530

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -3,6 +3,6 @@
 
 Follow_me_on_Youtube = "https://www.youtube.com/c/cvatricks"
 
-Live_Bot_Link = "https://t.me/PremiumSBBot"
+Live_Bot_Link = "http://t.me/Filessfi_bot"
 
 ################################################
